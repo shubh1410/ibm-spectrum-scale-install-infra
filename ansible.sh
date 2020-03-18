9d3defecf75ca84b95f60fe9da4207ffd590d03f
root <root@slitherfang-vm5.tuc.stglabs.ibm.com>
2020-03-17 23:35:13 -0700

    testing branch

--- ansible.sh
+++ ansible.sh
@@ -5,4 +5,4 @@ export ANSIBLE_LOG_PATH=/tmp/ansible_$(date "+%Y%m%d%H%M%S").log
 IFS=$','
 
 echo 'Running ansible-playbook -i hosts playbook.yml'
-ansible-playbook -i hosts playbook.yml
+ansible-playbook playbook.yml
